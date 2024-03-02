94e2f2dd4871bcf027cb9f69e97b7134c99289fb
Sabrina Rodrigues Fernandes <sabrinarfernandes.srf@gmail.com>
2024-03-01 22:20:15 -0300

    :sparkles: feat: routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('Carro','CarroController');
+Route::resource('Cliente','ClienteController');
+Route::resource('Locacao','LocadoraController');
+Route::resource('Marca','MarcaController');
+Route::resource('Modelo','ModeloController');
+
+
+
+
+
