eca056f4da292bc24a7d61ff8d39a86322a6dfb6
Sabrina Rodrigues Fernandes <sabrinarfernandes.srf@gmail.com>
2024-03-03 19:42:08 -0300

    :construction:  feat: request POST

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -18,16 +18,6 @@ class MarcaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -35,7 +25,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $marca = Marca::create($request->all());
+       return $marca;
     }
 
     /**
