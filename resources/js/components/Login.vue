a3232728370bbbd259a062c07e47cb7ea4236ef8
Sabrina Rodrigues Fernandes <sabrinarfernandes.srf@gmail.com>
2024-03-19 19:57:27 -0300

    :construction: feat: Implementando a autenticação web (sessão) e API (JWT)

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -4,14 +4,15 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Login (Componente VUE)</div>
+                <!--{{ email }}  -{{ password }} -->
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="" @submit.prevent="login($event)">
                         <input type="hidden" name="_token" :value="csrf_token" >
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="E-mail" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control" name="email" value="E-mail" required autocomplete="email" autofocus v-model="email">
 
                                
                             </div>
@@ -21,7 +22,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" v-model="password">
 
                             </div>
                         </div>
@@ -43,12 +44,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-
-                                
                                     <a class="btn btn-link" href="esqueceu a senha?">
                                         
                                     </a>
-                                
                             </div>
                         </div>
                     </form>
@@ -61,7 +59,36 @@
 
 <script>
     export default {
-        props: ['csrf_token']
+        props: ['csrf_token'],
+        data() {
+            return {
+                email: '',
+                password: ''
+            }   
+        },
+        methods: {
+            login(e){
+
+                let url = 'http://localhost:8000/api/login'
+                let configuracao = {
+                    method: 'post',
+                    body: new URLSearchParams( {
+                        'email' : this.email,
+                        'password' : this.password
+                    })
+                }
+
+                fetch(url,configuracao)
+                    .then(response => response.json())
+                    .then(data=>{
+                        if (data.token) {
+                            document.cookie = 'token='+data.token+':SameSite=Lax';
+                        }
+                        e.target.submit();//dar sequencia no envio do form de autenticação por sessão
+                    })
+                
+            }
+        }
     }
     
 </script>
