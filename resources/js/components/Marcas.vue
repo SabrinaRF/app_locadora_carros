559b9fd956ef7ead9ffa473c1f0f2c88a256e874
Sabrina Rodrigues Fernandes <sabrinarfernandes.srf@gmail.com>
2024-03-20 22:14:43 -0300

    :construction:  feat: Adição da view carros

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -1,4 +1,4 @@
-resources/js/components/ExampleComponent.vue<template>
+<template>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -7,6 +7,25 @@ resources/js/components/ExampleComponent.vue<template>
 
                     <div class="card-body">
                         Listagem das marcas.
+                        {{ id }} -{{ nome }}
+
+                        <form class="row g-3 needs-validation" novalidate method="POST" @submit.prevent="pesquisar($event)">
+                            <input type="hidden" name="_token" :value="csrf_token" ><!--cookie-->
+
+                            <div class="col-md-6">
+                                <label for="input_id" class="form-label">ID da Marca</label>
+                                <input type="text" class="form-control" id="input_id" value="Pesquisa pelo ID">
+                                
+                            </div>
+                            <div class="col-md-6">
+                                <label for="input_nome" class="form-label">Nome da Marca</label>
+                                <input type="text" class="form-control" id="input_nome" value="BMW" >
+                                
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary" type="submit">Pesquisar</button>
+                            </div>
+                            </form>
                     </div>
                 </div>
             </div>
@@ -15,5 +34,26 @@ resources/js/components/ExampleComponent.vue<template>
 </template>
 
 <script>
+    export default {
+        props: ['csrf_token'],
+        data(){
+            return {
+                input_id:'',
+                input_nome:''
+            }
+        },
+        method: {
+            pesquisar(e){
+                let url = 'http://localhost:8000/api/marcas'
+                let configuracao = {
+                    method: 'post',
+                    body: new URLSearchParams( {
+                        'id' : this.input_id,
+                        'nome' : this.input_nome
+                    })
+                }
+            }
+        }
+    }
 
 </script>
